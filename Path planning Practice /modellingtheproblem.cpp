50073ac2891bbcd25c250c59d008219c56a23479
Rafi S.M <33804907+rafism1997@users.noreply.github.com>
2022-05-23 15:14:48 +0530

    Completed the modelling problem

--- Path planning Practice /modellingtheproblem.cpp
+++ Path planning Practice /modellingtheproblem.cpp
@@ -20,17 +20,49 @@ public:
     };
 
 };
+class Planner : Map{
 
-int main(){
+    public: 
+        int start[2] ={0,0};
+        int goal[2] ={ mapHeight - 1 , mapWidth - 1 };
+        int cost =1;
 
-    Map map1;
-    for (vector<int> vect1D: map1.grid){
+        string movement_arrows[4] = {"^","<","v",">"};
 
-        for ( int x : vect1D)
-        {
-            cout<< x << " ";
+        vector<vector<int> > movements{
+        {-1,0},
+        {0,-1},
+        {1,0},
+        {0,1}
+
+        };    
+
+};
+
+template <typename T>
+void print2Dvector(T Vec)
+{
+    for(int i=0;i<Vec.size();++i){
+        for(int j=0;j < Vec[i].size();j++){
+            cout<<Vec[i][j] << ' ';
         }
         cout << endl;
     }
+}
+int main()
+{
+    Map map;
+    Planner planner;
+
+    cout<<"\n Map:"<<endl;
+    print2Dvector(map.grid);
+    
+    cout<< "\nStart: "<< planner.start[0] << "," << planner.start[1] << endl;
+    cout<< "\nGoal: "<<planner.goal[0]<<","<<planner.goal[1]<<endl;
+    cout<< "\nCost: "<<planner.cost << endl;
+    cout<<"\n Robot Movements: "<< planner.movement_arrows[0]<<","<<planner.movement_arrows[1]<<","<<planner.movement_arrows[1]<<","<<planner.movement_arrows[2]<<","<<planner.movement_arrows[3]<<endl;
+    cout<<"\nDelta: " << endl;
+    print2Dvector(planner.movements);
     return 0;
+
 }
